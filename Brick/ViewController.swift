a418fe377160703be601ebc9bc88fd0b8bf253a4
MrYang <BetrayalPromise@gmail.com>
2021-09-17 15:20:25 +0800

    Initial Commit

---
+++ Brick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Brick
+//
+//  Created by 李阳 on 17/9/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
